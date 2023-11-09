41efe766b5152410d8e1194cc8b030004abb0979
Simon Olofsson <telsak@gmail.com>
2023-11-09 23:08:47 +0100

    Switchover from old project

---
+++ canvasmagic.py
@@ -0,0 +1,94 @@
+import requests
+
+base = 'https://hv.instructure.com/api/v1/'
+
+def check_canvas_token(token):
+    """
+    Check if the provided Canvas API token matches the one stored in the `canvas.crd` file.
+
+    Args:
+        token (str): The Canvas API token to verify.
+
+    Returns:
+        bool: True if the token matches, False otherwise.
+    """
+    try:
+        with open('canvas.crd') as file:
+            return token == file.read()
+    except:
+        return False
+
+def get_course_assignments(course_id, token):
+    """
+    Retrieve a list of assignments for a specific course, excluding any with 'kursvärdering' in the name.
+
+    Args:
+        course_id (str): The identifier for the Canvas course.
+        token (str): The Canvas API token for authentication.
+
+    Returns:
+        list: A list of tuples containing the assignment name, id, and points possible.
+    """
+    url = f'{base}courses/{course_id}/assignments'
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get(url, headers=headers).json()
+    assignments = []
+    for assignment in response:
+        if 'kursvärdering' not in assignment['name'].lower():
+            assignments.append((assignment['name'], assignment['id'], assignment['points_possible']))
+    return assignments
+
+def get_student_info(token, course_id, login_id):
+    """
+    Retrieve the student ID associated with a specific login ID for a course.
+
+    Args:
+        token (str): The Canvas API token for authentication.
+        course_id (str): The identifier for the Canvas course.
+        login_id (str): The login ID associated with the student.
+
+    Returns:
+        tuple (str, str): (The student ID, The student name)
+    """
+    url = f'{base}courses/{course_id}/search_users'
+    headers = {"Authorization": f"Bearer {token}"}
+    payload = {"search_term": f"{login_id}"}
+    response = requests.get(url, data=payload, headers=headers).json()
+    return (response[0]["id"], response[0]["name"])
+
+def get_course_info(token, course_id):
+    """
+    Retrieve information about a specific Canvas course by course ID.
+
+    Args:
+        token (str): The API token for authentication.
+        course_id (str or int): The identifier of the course to retrieve information for.
+
+    Returns:
+        dict: A dictionary containing the course information in JSON format.
+    """
+    url = f'{base}courses/{course_id}'
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get(url, headers=headers).json()
+    return response
+
+def set_assignment_completion(token, course_id, assignment, student_id):
+    """
+    Mark an assignment as completed for a specific student by assigning full points.
+
+    Args:
+        token (str): The Canvas API token for authentication.
+        course_id (str): The identifier for the Canvas course.
+        assignment (tuple): A tuple containing the assignment details (name, id, points possible).
+        student_id (str): The student ID.
+
+    Returns:
+        dict: The response from the Canvas API after updating the assignment submission.
+    """
+    assignment_id = assignment[0]
+    assignment_points = assignment[1]
+    url = f'{base}courses/{course_id}/assignments/{assignment_id}/submissions/{student_id}'
+    headers = {"Authorization": f"Bearer {token}"}
+    payload = {"submission[posted_grade]": f"{assignment_points}"}
+    response = requests.put(url, data=payload, headers=headers)
+    return (response.status_code == 200, response.json())
---
+++ main.py
@@ -0,0 +1,113 @@
+import json
+from flask import Flask, request, jsonify, post
+from canvasmagic import (
+    check_canvas_token, get_course_assignments, get_student_info,
+    get_course_info, set_assignment_completion
+)
+
+app = Flask(__name__)
+NFC_DATA_FILE = 'nfc_data.json'
+
+def read_config():
+    with open('config.json', 'r') as file:
+        return json.load(file)
+
+def read_nfc_data(file_path=NFC_DATA_FILE):
+    """Reads NFC data from a JSON file.
+
+    Args:
+        file_path: A string path to the JSON file to be read.
+
+    Returns:
+        A tuple containing:
+        - A boolean indicating the success status of the operation.
+        - A dictionary with the contents read from the file, or an empty dict
+          if an error occurs.
+        - A string message indicating the result of the operation.
+    """
+    try:
+        with open(file_path, 'r') as nfc_file:
+            return json.load(nfc_file)
+    except FileNotFoundError:
+        return dict()
+    except json.JSONDecodeError as e:
+        return dict()
+
+def write_nfc_data(nfc_data, file_path=NFC_DATA_FILE):
+    """Writes NFC data to a JSON file.
+
+    Args:
+        nfc_data: A dictionary containing NFC data to be written to the file.
+        file_path: A string path to the JSON file to be written.
+
+    Returns:
+        A tuple containing:
+        - A boolean indicating the success status of the write operation.
+        - A string message indicating the result of the operation.
+    """
+    try:
+        with open(file_path, 'w') as file:
+            json.dump(nfc_data, file, indent=2)
+            return True
+    except IOError as e:
+        return False
+
+app.config['SETTINGS'] = read_config()
+
+def log_webhook(msg='Null'):
+    url = app.config['SETTINGS']['base']['logging']['WEBHOOK']
+    post(url, json={"username": 'autograde: NFC_API-Grade', "content": msg})
+
+app.config['BASE'] = 'https://hv.instructure.com/api/v1/'
+app.config['NFC_DATA'] = read_nfc_data()
+
+@app.route("/nfc", methods=['POST'])
+def nfc():
+    response = request.json
+    token = response["token"]
+
+    if check_canvas_token(token):
+        nfc_users = app.config['NFC_DATA']
+        data = response["data"]
+        if response["action"] == "check":
+            if data in nfc_users:
+                return jsonify({"status": "check_success", "data": nfc_users[data]}), 200
+            else:
+                return jsonify({"status": "success", "data": "Unknown ID"}), 200
+        elif response["action"] == "register":
+            full_name = get_student_info(token, response["payload"][1], response["payload"][0])[1]
+            payload = {
+                "login_id": response["payload"][0],
+                "name": full_name
+            }
+            app.config['NFC_DATA'][data] = payload
+            if write_nfc_data(app.config['NFC_DATA']):
+                return jsonify({"status": "register_success", "data": nfc_users[data]}), 200
+            else:
+                return jsonify({"status": "register_failed", "data": nfc_users[data]}), 500
+        elif response["action"] == "get_labs":
+            course_id = response["data"]
+            assignments = get_course_assignments(course_id, token)
+            return jsonify({"status": "query_success", "data": assignments}), 200
+        elif response["action"] == "mark_completed":
+            data = response["payload"]
+            course_id = data["course"]
+            student_id = get_student_info(token, course_id, nfc_users[response["data"]]["login_id"])[0]
+            assignment = (data["assignment"]["id"], str(data["assignment"]["points"]))
+            canvas_response = set_assignment_completion(token, course_id, assignment, student_id)
+            if canvas_response[0] == True:
+                course_name = get_course_info(token, course_id)["name"]
+                log_webhook(
+                    facility='NFC_API', 
+                    msg=f'{nfc_users[response["data"]]["name"]} : User clear lab {assignment}, course {course_name}'
+                )
+                return jsonify({"status": "mark_completed_success", "data": assignment}), 200
+            else:
+                return jsonify({"status": "failed", "data": "Failed to mark Lab Complete"})
+        else:
+            return jsonify({"status": "forbidden", "data": "Invalid Action"}), 400
+    else:
+        return jsonify({"status": "forbidden", "data": "Invalid Token Provided"}), 403
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
